cd4122800fc73643ed5d84147d00b7e2b699a4e9
NetasDev <tokae100@uni-duesseldorf.de>
2021-02-27 15:24:03 +0100

    added function to edit dotdics

--- utils.py
+++ utils.py
@@ -20,3 +20,5 @@ class AverageMeter(object):
 class dotdict(dict):
     def __getattr__(self, name):
         return self[name]
+    def __setattr__(self,key,value):
+        self.__setitem__(key,value)
