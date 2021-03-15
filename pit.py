7295ed3fc283ab6f671b4ba0a1045978811fadb3
NetasDev <tokae100@uni-duesseldorf.de>
2021-03-15 19:20:00 +0100

    added function to play 1 player against many nn's

--- pit.py
+++ pit.py
@@ -21,8 +21,12 @@ TK First an object of the game has to be created and atleast 2 Players of that g
 game = OthelloGame(8)
 hp = HumanOthelloPlayer(game)
 greed = GreedyOthelloPlayer(game)
-minimax = MinimaxOthelloPlayer(game,10,maxtime=3)
-minimax2 = MinimaxOthelloPlayer(game,10,maxtime=3)
+minimax = MinimaxOthelloPlayer(game,4)
+minimax.name = "valueMatrix"
+minimax2 = MinimaxOthelloPlayer(game,4,mode=1)
+minimax2.name = "baseMinimax"
+minimax3 = MinimaxOthelloPlayer(game,4,mode=2)
+minimax3.name = "betterFunction"
 """
 TK In Order to create a neural Network player with MCTS a NNetWrapper has to be created and this Wrapper has to load the net 
 from the target folder with the given filename
@@ -52,7 +56,7 @@ X has to be a multiple of 2
 The variable Interactive can be set to True to have an interactive board shown on screen.
 This Board shows the current board, the possible moves of the current player und more information about the match.
 """
-player1wins,player2wins,draws = arena.playGames(10,Interactive=True,save="./newFolder/testgames2/")
+player1wins,player2wins,draws = arena.playGames(80,Interactive=True,save="./newFolder/testgames2/")
 print(arena.player1.name + " : "+ str(player1wins))
 print(arena.player2.name + " : "+ str(player2wins))
 print("draws : " +str(draws))
@@ -69,13 +73,13 @@ TK There is also the Option to play a tournament between two or more players
 If a save path is set for the tournament,there will be new folders created with the names of the two players
 matched against each other and the games will be saved in there.
 """
-
+"""
 players = []
 players.append(greed)
 players.append(minimax)
 players.append(minimax2)
 Arena.Arena.play_tournament(players,2,game,2,savefolder="./newFolder/tournament")
-
+"""
 
 
 
